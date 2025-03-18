c1f51891cc199c4815ee9dd4738eaa227fc6b1a5
Zach Berritt <zberritt@gmail.com>
2025-03-17 21:37:14 -0400

    Better trash generation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    private List<TrashData> inventory;
 
     // Add any sort of save data here
     public int boatUpgradeLevel = 0;
@@ -17,5 +20,26 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+        inventory = new List<TrashData>();
     }
+
+    public bool TrashCollected(TrashData item)
+    {
+        if (InventoryCapacity() == InventoryUsed())
+            return false;
+        inventory.Add(item);
+        Debug.Log("Collected" + item.Name);
+        return true;
+    }
+
+    public int InventoryCapacity()
+    {
+        return (boatUpgradeLevel + 1) * 10;
+    }
+
+    public int InventoryUsed()
+    {
+        return inventory.Count;
+    }
+
 }
